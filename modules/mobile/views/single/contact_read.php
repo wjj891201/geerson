0eba533966c74297e0eb99fffdcba9519cf0f589
wujiepeng <517987404@qq.com>
2019-02-26 17:10:32 +0800

    优化

---
+++ modules/mobile/views/single/contact_read.php
@@ -0,0 +1,20 @@
+<?php
+
+use yii\helpers\Url;
+
+$this->title = $info['headtitle'];
+$this->params['breadcrumbs'][] = $this->title;
+$this->registerMetaTag(['name' => 'keywords', 'content' => $info['keywords']]);
+$this->registerMetaTag(['name' => 'description', 'content' => $info['description']], 'description');
+?>
+<div class="w100">
+    <img src="<?= $type['typepic'] ?>" />
+</div>
+<div class="wrap">
+    <div class="article_cont">
+        <?= $info['content']['content'] ?>
+    </div>
+</div>
+<style>
+    img{width: 100%;}
+</style>
