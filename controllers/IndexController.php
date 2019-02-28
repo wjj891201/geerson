ac1e70e3be5ae44ef5996de1fc20277401f04f6b
wujiepeng <517987404@qq.com>
2019-02-28 11:20:53 +0800

    移动端跳转优化

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -5,12 +5,18 @@ namespace app\controllers;
 use Yii;
 use app\controllers\CommonController;
 use app\models\Advert;
+use app\libs\Tool;
 
 class IndexController extends CommonController
 {
 
     public function actionIndex()
     {
+        $isMobile = Tool::isMobile();
+        if ($isMobile)
+        {
+            return $this->redirect(['mobile/index/index']);
+        }
         $banner = Advert::get_ad(['atid' => 1]);
         return $this->render("index", ['banner' => $banner]);
     }
